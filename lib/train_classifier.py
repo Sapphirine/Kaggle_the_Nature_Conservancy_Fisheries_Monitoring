a1b8698e078d0dac4f029c3b461ca33540f586ca
PengfeiWangWZ <hehtihawa@gmail.com>
2016-12-02 11:58:47 -0500

    Fix minor bug

--- lib/train_classifier.py
+++ lib/train_classifier.py
@@ -20,19 +20,23 @@ neg_name = os.listdir(neg_path)
 
 # resize and save
 for i, j in enumerate(pos_name):
+    train = []
     train[:] = imresize(imread(j), [128, 128, 3])
     imsave(j, train)
     with open('/Users/pengfeiwang/Desktop/f/data/output/pos.info', 'ab') as g:
         g.write(j + ' 1 0 0 128 128 \n')
 
 for i in neg_name:
+    i_name = neg_path + i
+    neg = []
+    neg[:] = imresize(imread(i_name), [128, 128, 3])
+    imsave(i_name, train)
     with open('/Users/pengfeiwang/Desktop/f/data/output/neg.info', 'ab') as g:
-        g.write(i + '\n')
+        g.write(i_name + '\n')
 
 # in bash
-# opencv_createsamples -info /Users/pengfeiwang/Desktop/f/data/output/pos.info -num ? -w 128 -h 128 -vec /Users/pengfeiwang/Desktop/f/data/output/fish.vec
-# opencv_createsamples -vec /Users/pengfeiwang/Desktop/f/data/output/fish.vec -w 128 -h 128
-# opencv_traincascade -data output/file/dir -vec \
-# /Users/pengfeiwang/Desktop/f/data/output/fish.vec -bg \
-# /Users/pengfeiwang/Desktop/f/data/output/neg.info -numPos ? -numNeg ? -w \
-# 128 -h 128 -featureType LBP
+# cd into the location of pos.info
+# opencv_createsamples -info pos.info -num 4471 -w 128 -h 128 -vec /Users/pengfeiwang/Desktop/f/data/output/fish.vec
+# opencv_createsamples -vec fish.vec -w 128 -h 128
+# opencv_traincascade -data data -vec fish.vec -bg neg_info.txt -numStages 10 -numPos 4471 -numNeg 465 -w 128 -h 128 -featureType LBP
+
