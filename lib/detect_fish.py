13dfc6e836d8290064a7ed7a158770bee9a8902c
PengfeiWangWZ <hehtihawa@gmail.com>
2016-12-06 15:54:07 -0500

    Minor change

--- lib/detect_fish.py
+++ lib/detect_fish.py
@@ -1,7 +1,8 @@
-import numpy as np
-from scipy.misc import *
 import cv2
 import os
+import numpy as np
+from scipy.misc import *
+import matplotlib.pyplot as plt
 
 os.chdir('/Users/pengfeiwang/Desktop/f/fisheries/')
 fish_cascade = cv2.CascadeClassifier('./output/redfish.xml')
@@ -17,9 +18,11 @@ for i in file_name:
 		img = imresize(cv2.imread(i), [64, 64, 3])
 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		fish = fish_cascade.detectMultiScale(gray, 1.3, 5)
-		for (x,y,w,h) in fish:
+		for j, (x,y,w,h) in enumerate(fish):
 		    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)  # color thickness
-		    cv2.imshow('img',img)
 		    crop_fish = img[y:y+h, x:x+w]
-		    cv2.imwrite(os.path.join(output_path,output_name),crop_fish,)
-		    
+		    cv2.imwrite(os.path.join(output_path,str(j)+'_'+output_name),crop_fish,)
+		cv2.imshow('img',img)
+	break
+
+plt.show()		    
