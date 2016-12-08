d1bb8a1bcc55f0414196be8d9dcad66ecf9c79e0
lesson1221 <lesson1221@gmail.com>
2016-12-07 21:47:57 -0500

    Add files via upload

---
+++ lib/crop.py
@@ -0,0 +1,63 @@
+# import the necessary packages
+import argparse
+import cv2
+ 
+# initialize the list of reference points and boolean indicating
+# whether cropping is being performed or not
+refPt = []
+cropping = False
+ 
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping
+ 
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+ 
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+ 
+		# draw a rectangle around the region of interest
+		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.imshow("image", image)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_and_crop)
+
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("image", image)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the 'r' key is pressed, reset the cropping region
+	if key == ord("r"):
+		image = clone.copy()
+ 
+	# if the 'c' key is pressed, break from the loop
+	elif key == ord("c"):
+		break
+ 
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+if len(refPt) == 2:
+	#roi = cv2.rectangle(image,refPt[0], refPt[1],(255,255,0),2)
+	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+	cv2.imshow("ROI", roi)
+	cv2.imwrite("/Users/Kevin/Desktop/test_data/test_crop/1.jpg",roi,)
+	cv2.waitKey(0)
+    
+cv2.destroyAllWindows()
