6b1f62dc30165cd3df3c85cffaf8d22a89d82101
PengfeiWangWZ <hehtihawa@gmail.com>
2016-12-08 00:18:11 -0500

    Fix

--- lib/crop_test_fish.py
+++ lib/crop_test_fish.py
@@ -1,63 +1,48 @@
-# import the necessary packages
-import argparse
+import os
 import cv2
  
-# initialize the list of reference points and boolean indicating
-# whether cropping is being performed or not
-refPt = []
-cropping = False
  
 def click_and_crop(event, x, y, flags, param):
-	# grab references to the global variables
 	global refPt, cropping
- 
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being
-	# performed
 	if event == cv2.EVENT_LBUTTONDOWN:
 		refPt = [(x, y)]
 		cropping = True
- 
-	# check to see if the left mouse button was released
 	elif event == cv2.EVENT_LBUTTONUP:
-		# record the ending (x, y) coordinates and indicate that
-		# the cropping operation is finished
 		refPt.append((x, y))
 		cropping = False
- 
-		# draw a rectangle around the region of interest
 		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
 		cv2.imshow("image", image)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
+image_path = '/Users/pengfeiwang/Desktop/f/data/test_stg1'
+output_path = '/Users/pengfeiwang/Desktop/output_test'
+absolute_path = [os.path.join(image_path,i) for i in os.listdir(image_path) if i[0] != '.'] 
 
-while True:
-	# display the image and wait for a keypress
-	cv2.imshow("image", image)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the 'r' key is pressed, reset the cropping region
-	if key == ord("r"):
-		image = clone.copy()
- 
-	# if the 'c' key is pressed, break from the loop
-	elif key == ord("c"):
-		break
- 
-# if there are two reference points, then crop the region of interest
-# from teh image and display it
-if len(refPt) == 2:
-	#roi = cv2.rectangle(image,refPt[0], refPt[1],(255,255,0),2)
-	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-	cv2.imshow("ROI", roi)
-	cv2.imwrite("/Users/Kevin/Desktop/test_data/test_crop/1.jpg",roi,)
-	cv2.waitKey(0)
-    
-cv2.destroyAllWindows()
+
+for i in absolute_path:
+	image = cv2.imread(i)
+	clone = image.copy()
+	cv2.namedWindow("image")
+	refPt = []
+	cropping = False
+	cv2.setMouseCallback("image", click_and_crop)
+	while True:
+		cv2.imshow("image", image)
+		key = cv2.waitKey(1) & 0xFF
+		# if the 'r' key is pressed, reset the cropping region
+		if key == ord("r"):
+			image = clone.copy()
+		# if the 'c' key is pressed, break from the loop
+		elif key == ord("c"):
+			break
+	 
+	if len(refPt) == 2:
+		print refPt[0][1],refPt[1][1]
+		print refPt[0][0],refPt[1][0]
+		roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+		cv2.imshow("ROI", roi)
+		cv2.imwrite(os.path.join(output_path,absolute_path.split('/')[-1]),roi)
+		cv2.waitKey(0)
+	    
+	cv2.destroyAllWindows()
+
